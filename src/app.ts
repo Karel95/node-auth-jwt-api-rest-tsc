2523b74e0e166ea8331b76c091ece12ae7aa9e8a
Karel <karelh2207@gmail.com>
2024-10-19 16:05:07 -0300

    updating

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import dotenv from 'dotenv';
 dotenv.config()
 import express from 'express';
 const app = express()
+const cors = require('cors');
 import authRoutes from './routes/authRoutes';
 import usersRoutes from './routes/userRoutes';
 
 // Middleware for parsing JSON request bodies
 app.use(express.json())
+
+// Permitir todas las solicitudes de todos los orígenes
+app.use(cors());
+
+// O permitir solo tu dominio de frontend
+// app.use(cors({
+//   origin: 'http://localhost:5173'  // Cambia por tu dominio en producción si es necesario
+// }));
 
 // Routes:
 //Authentication:
