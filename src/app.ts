4e593f6b8749bdc12b5889caa90aff303e5c1908
Karel <karelh2207@gmail.com>
2024-10-19 21:54:18 -0300

    updating

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,34 @@
-import dotenv from 'dotenv';
-dotenv.config()
-import express from 'express';
-const app = express()
-const cors = require('cors');
-import authRoutes from './routes/authRoutes';
-import usersRoutes from './routes/userRoutes';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+const cors = require("cors");
+import authRoutes from "./routes/authRoutes";
+import usersRoutes from "./routes/userRoutes";
 
 // Middleware for parsing JSON request bodies
-app.use(express.json())
+app.use(express.json());
 
 // Permitir todas las solicitudes de todos los orígenes
 app.use(cors());
 
 // O permitir solo tu dominio de frontend
-app.use(cors({
-  origin: 'http://localhost:5173'  // Cambia por tu dominio en producción si es necesario
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", // Local development
+      "https://eduweb-suriname.netlify.app", // Production domain
+    ],
+  })
+);
 
 // Routes:
 //Authentication:
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
 
 //User. Api rest:
-app.use('/users', usersRoutes)
+app.use("/users", usersRoutes);
 
-console.log("Hello world")
+console.log("Hello world");
 
-export default app
+export default app;
